ea9184600b8eeda9b5e653a191f8b0561d34d88a
cscheve3 <cscheve3@gmail.com>
2023-12-04 22:43:09 -0600

    input and setup to read file

--- d3-go/main.go
+++ d3-go/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Create a scanner to read the file
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		// line := scanner.Text()
+	}
 }
